2c77c2e13a114ec06c2d5d4d406b80af22fc7db6
Bram-Boris Meerlo <me@bramboris.nl>
2019-11-20 16:53:52 +0100

    Commented my hard to understand code.

--- lib/systems/game_system.cpp
+++ lib/systems/game_system.cpp
@@ -14,19 +14,27 @@ void GameSystem::update(double deltatime) {
             dead_players.insert(player.first);
         }
     }
-
+    
+    // If there are any dead players.
     if(!dead_players.empty()) {
+        // If only one player is alive.
         int count = players.size() - dead_players.size();
         if(count <= 1) {
-            // load next scene.
+            // Count up to 1
             timer += deltatime;
+            
+            // Intermission should show all numbers. You can not call it only in the timer because then it would count down from 2.
             if(seconds == 3)
                 game_controller.intermission(seconds);
+            
+            // If the timer reached above 1 reset it. Update the intermission screen with the new view. 
             if (timer > 1) {
                 timer = 0;
                 seconds--;
                 game_controller.intermission(seconds);
             }
+            
+            // Load the new level and reset all timers.
             if(seconds <= 0) {
                 timer = 0;
                 seconds = 3;
