4f4d82c913c1e6590353ef83d131536e1fb03fcc
Luuk Santegoeds <santegoedsweb@gmail.com>
2019-11-23 11:25:50 +0100

    Added a cout on modifier change

--- lib/systems/game_speed_system.cpp
+++ lib/systems/game_speed_system.cpp
@@ -13,14 +13,22 @@ void GameSpeedSystem::update(double delta_time) {
 
     auto input = BrickInput<PlayerInput>::getInstance();
     double step_size = 0.2;
+    bool modifier_changed = false;
 
     if(input.checkInput(1, PlayerInput::PAGE_DOWN)) {
         // Decrease game speed
-        if(*delta_time_modifier - step_size >= 0.2)
+        if(*delta_time_modifier - step_size >= 0.2) {
             *delta_time_modifier -= 0.2;
+            modifier_changed = true;
+        }
     } else if (input.checkInput(1, PlayerInput::PAGE_UP)) {
         // Increase game speed
-        if(*delta_time_modifier + step_size <= 3)
+        if(*delta_time_modifier + step_size <= 3) {
             *delta_time_modifier += 0.2;
+            modifier_changed = true;
+        }
     }
+
+    if(modifier_changed)
+        std::cout << "New game loop speed modifier = " << *delta_time_modifier << std::endl;
 }
