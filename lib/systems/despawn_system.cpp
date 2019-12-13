222994ef7d2c23b49895904e6aade68618eb3565
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-12-14 00:00:44 +0100

    Fixed bullets disappearing in your own belly, yum.
    Co-authored-by: JanSchollaert <jbscholl@avans.nl>

--- lib/systems/despawn_system.cpp
+++ lib/systems/despawn_system.cpp
@@ -3,6 +3,7 @@
 #include "brickengine/components/player_component.hpp"
 #include "components/health_component.hpp"
 #include "components/stats_component.hpp"
+#include "components/damage_component.hpp"
 
 #include "systems/despawn_system.hpp"
 
@@ -63,9 +64,11 @@ void DespawnSystem::update(double) {
         if (despawn_comp->despawn_on_collision) {
             auto collisions = collision_detector.detectCollision(entity_id);
             if (!collisions.empty()) {
+                auto damage = entityManager->getComponent<DamageComponent>(entity_id);
                 bool should_despawn = false;
                 for (auto& collision : collisions) {
                     if (!collision.is_trigger) {
+                        if (damage && damage->damage_dealer_entity_id == collision.opposite_id) continue;
                         should_despawn = true;
                         auto opposite_despawn = entityManager->getComponent<DespawnComponent>(collision.opposite_id);
                         if (opposite_despawn && opposite_despawn->despawn_on_collision)
